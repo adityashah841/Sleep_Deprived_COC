032a7f011272279d629f43084cc694d93ab33ce9
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2023-03-04 15:31:15 +0530

    Created Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Main from './components/Main';
 import { Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <Navbar className="Navbar"/>
+      <Main/>
       <Routes>
         {/* <Route path="/" element={<Home/>}/> */}
         {/* <Route path="/about" element={<About/>}/> */}
