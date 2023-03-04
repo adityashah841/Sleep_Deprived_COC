a0006de619e321ee31deaff7f69ecc55965ab3b6
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2023-03-04 23:02:47 +0530

    Created Register Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Main from './components/Main';
 import { Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import { Routes } from 'react-router-dom';
+import CreateAccount from './elements/CreateAccount';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/app" element={<Main/>}/>
         <Route path="/" element={<Landing/>}/>
+        <Route path="/create-account" element={<CreateAccount/>}/>
       </Routes>
     </div>
   );
