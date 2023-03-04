f51d7a6bed5366d129dbdec3ff809e850a42a01d
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2023-03-04 15:12:52 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Navbar from './Components/Navbar';
+import { Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      Hi
+      <Navbar className="Navbar"/>
+      <Routes>
+        {/* <Route path="/" element={<Home/>}/> */}
+        {/* <Route path="/about" element={<About/>}/> */}
+      </Routes>
     </div>
   );
 }
