3fdd6afddca114055e3f96e050cf4856843a0d50
Siddhant Shah <sidrocks2909@gmail.com>
2023-03-04 18:09:56 +0530

    Improvement in sidebar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
-import SideBar from './Components/SideBar';
-import Navbar from './components/Navbar';
+import SideBar from './components/SideBar';
+import Content from './components/Content';
+// import Navbar from './components/Navbar';
 import { Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <div className="sideBar">
-        <SideBar />
-      </div>
-      <Navbar className="Navbar"/>
-      <Routes>
-        {/* <Route path="/" element={<Home/>}/> */}
-        {/* <Route path="/about" element={<About/>}/> */}
-      </Routes>
+    <div className="app">  
+      <nav>
+        <div className="sideBar">
+          <SideBar className="SideBar"/>
+        </div>
+        <div className="content">
+          <Content className="content"/>
+        </div>
+        <Routes>
+          {/* <Route path="/" element={<Home/>}/> */}
+          {/* <Route path="/about" element={<About/>}/> */}
+        </Routes>
+      </nav>
     </div>
   );
 }
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -4,12 +4,53 @@ import '../css/SideBar.css'
 function SideBar() {
     return (
         <div>
+            <nav className="nav">
+            <button className="profile" /*onClick={() => console.log('Clicked')}*/>
+                Click Me!
+            </button>
+            <button className="security" /*onClick={() => console.log('Clicked')}*/>
+                Security!
+            </button>
+            <button className="explore" /*onClick={() => console.log('Clicked')}*/>
+                Explore!
+            </button>
+            </nav>
             <h2>
                 SideBar for Profile and Settings
             </h2>
-            <button id="Profile" onClick={() => console.log('Clicked')}>
-                Click Me!
-            </button>
+            <div className="subscription">
+                Cupid Pro
+            </div>
+            <div className="subscription">
+                Cupid Plus
+            </div>
+            <div className="account settings">
+                <h2>Account Settings</h2>
+                <div>
+                    1. email
+                </div>
+                <div>
+                    2. phone number
+                </div>
+            </div>
+            <div classname="discovery settings">
+                <h2>Discovery Settings</h2>
+                <div>
+                    <input type="range" name="distance within" min="" max=""/>
+                </div>
+            </div>
+            <div classname="global settings">
+                <h2>Global Settings</h2>
+            </div>
+            <div classname="Help & Support">
+                <h2>Help & Support</h2>
+            </div>
+            <div classname="Safety">
+                <h2>Safety</h2>
+            </div>
+            <div classname="Legal">
+                <h2>Legal</h2>
+            </div>
         </div>
     )
 }
